ed0a6d968b5aa50022c7a4dd039a8a62b438ea5d
Cherish  Puniani <cherishpuniani2014@gmail.com>
2026-08-23 12:12:41 +0530

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.py
@@ -0,0 +1,16 @@
+# To reduce Tc we can solve this without sorting so taking out the nLogn step
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numS = set(nums)
+        ans = 0
+        for num in numS: # instead of iterating nums
+            if num-1 not in numS:
+                l = 1
+                while num+1 in numS:
+                    l += 1
+                    num += 1
+                ans = max(ans,l)
+            else:
+                continue
+        return ans
+        
