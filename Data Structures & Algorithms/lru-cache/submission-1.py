7b4b423de33ee32cd1f62480794a8c0f226e4437
Cherish  Puniani <cherishpuniani2014@gmail.com>
2026-08-25 23:24:50 +0530

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, key=0, val=0):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}  # key -> Node
+        # Dummy head and tail nodes
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node: Node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def _add_to_tail(self, node: Node):
+        node.prev = self.tail.prev
+        node.next = self.tail
+        self.tail.prev.next = node
+        self.tail.prev = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self._remove(node)
+        self._add_to_tail(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        
+        node = Node(key, value)
+        self._add_to_tail(node)
+        self.cache[key] = node
+
+        if len(self.cache) > self.capacity:
+            # Evict LRU from head
+            lru = self.head.next
+            self._remove(lru)
+            del self.cache[lru.key]
